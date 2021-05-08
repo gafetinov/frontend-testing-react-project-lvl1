a81d08e7c5c330df5d599741cafe3b9372045be6
gafetinov <artem.gafetinov@gmail.com>
2021-05-08 21:58:42 +0500

    Add pictures upload

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,9 +7,13 @@ import loadPage from '../src';
 describe('page-loader', () => {
   let dirToSave;
 
+  beforeAll(() => {
+    nock.disableNetConnect();
+  });
+
   beforeEach((done) => {
     nock.cleanAll();
-    fs.mkdtemp(path.join(os.tmpdir(), 'page-loader'), (err, dir) => {
+    fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'), (err, dir) => {
       dirToSave = dir;
       done();
     });
@@ -31,18 +35,23 @@ describe('page-loader', () => {
     const jpegPath = '/images/picture.jpeg';
     const pageContent = fs.readFileSync(`${__dirname}/../__fixtures__/page.html`, 'utf-8');
     const png = fs.readFileSync(`${__dirname}/../__fixtures__/page_files/picture.png`, 'binary');
-    const jpeg = fs.readFileSync(`${__dirname}/../__fixtures__/page_files/picture.jpeg`, 'binary');
-    nock(pageHref).get('/').reply(200, pageContent);
-    nock(pageHref).get(pngPath).reply(200, png);
-    nock(pageHref).get(jpegPath).reply(200, jpeg);
+    const jpeg = fs.readFileSync(`${__dirname}/../__fixtures__/page_files/picture.jpg`, 'binary');
+    const scope = nock(pageHref)
+      .get('/')
+      .reply(200, pageContent)
+      .get(pngPath)
+      .reply(200, png)
+      .get(jpegPath)
+      .reply(200, jpeg);
     const resultPath = await loadPage(pageHref, dirToSave);
+    expect(scope.isDone()).toBe(true);
     expect(resultPath).toBe(`${dirToSave}/www-example-com.html`);
     const loadedPageContent = fs.readFileSync(resultPath, 'utf-8');
     const expectedContent = fs.readFileSync(`${__dirname}/../__fixtures__/loaded-page.html`, 'utf-8');
     expect(loadedPageContent).toBe(expectedContent);
-    const loadedPng = fs.readFileSync(`${resultPath}/www-example-com_files/picture.png`, 'binary');
+    const loadedPng = fs.readFileSync(`${dirToSave}/www-example-com_files/picture.png`, 'binary');
     expect(loadedPng).toEqual(png);
-    const loadedJpeg = fs.readFileSync(`${resultPath}/www-example-com_files/picture.jpeg`, 'binary');
-    expect(loadedJpeg).toEqual(loadedJpeg);
+    const loadedJpeg = fs.readFileSync(`${dirToSave}/www-example-com_files/picture.jpg`, 'binary');
+    expect(loadedJpeg).toEqual(jpeg);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
 import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
+import cheerio from 'cheerio';
 
-const getPageFilePath = (href, dirToSave) => {
-  const fileName = href.replace(/^https?:\/\//, '').replace(/\W/g, '-');
-  return `${path.resolve(dirToSave)}/${fileName}.html`;
+const getPage = (href) => axios.get(href).then((res) => res.data);
+const loadPicture = (href, pathToSave) => axios({
+  method: 'get',
+  url: href,
+  responseType: 'stream',
+}).then((response) => {
+  response.data.pipe(fs.createWriteStream(pathToSave));
+});
+
+const findAllImgTags = (html) => {
+  const $ = cheerio.load(html);
+  const imgTagsString = cheerio.html($('img'));
+  const separator = '>';
+  return imgTagsString
+    .split(separator)
+    .filter((x) => !!x && x !== '')
+    .map((element) => element + separator);
 };
 
+const replaceSrcToLocal = (html, imgTags, root) => imgTags.reduce((acc, tag) => {
+  const src = tag.match(/src="(.+)"/i)[1];
+  const imgName = src.split('/').pop();
+  const tagWithLocalSrc = tag.replace(`src="${src}"`, `src="${root}/${imgName}"`);
+  return acc.replace(tag, tagWithLocalSrc);
+}, html);
+
 export default async (href, dirToSave = '.') => {
-  const pageFilePath = getPageFilePath(href, dirToSave);
-  await axios.get(href).then(({ data }) => {
-    fs.writeFileSync(pageFilePath, data);
-  }).catch(() => {
-    throw new Error('Failed to load page');
-  });
-  return pageFilePath;
+  const commonFilesName = href.replace(/^https?:\/\//, '').replace(/\W/g, '-');
+  const root = path.resolve(dirToSave);
+  const htmlPath = `${root}/${commonFilesName}.html`;
+  const resourcesDirPath = `${root}/${commonFilesName}_files`;
+  const pageContent = await getPage(href);
+  const imgTags = findAllImgTags(pageContent);
+  if (imgTags.length > 0 && !fs.existsSync(resourcesDirPath)) {
+    fs.mkdirSync(resourcesDirPath);
+  }
+  const imgUrls = imgTags.map((tag) => `${href}${tag.match(/src="(.+?)"/i)[1]}`);
+  await Promise.all(imgUrls.map((url) => {
+    const imgName = url.split('/').pop();
+    return loadPicture(url, `${resourcesDirPath}/${imgName}`);
+  }));
+  const updatedHtml = replaceSrcToLocal(pageContent, imgTags, `/${commonFilesName}_files`);
+  fs.writeFileSync(htmlPath, updatedHtml, 'utf-8');
+  return htmlPath;
 };
